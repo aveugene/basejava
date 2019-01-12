5fd425e3260afc69141178e696455b18fad6f92c
aveugene <eugeneavilov@gmail.com>
2019-01-12 09:51:30 +0700

    Lesson08. HW08_2 Print all files and folders via recursive method.

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -1,11 +1,12 @@
 package ru.javawebinar.basejava;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 public class MainFile {
     public static void main(String[] args) {
+
+        /*
         String filePath = ".\\.gitignore";
 
         File file = new File(filePath);
@@ -28,6 +29,34 @@ public class MainFile {
             System.out.println(fis.read());
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+        */
+
+        File directory = new File(".");
+        listDirectory(directory);
+    }
+
+    private static void listDirectory(File directory) {
+        String[] list = directory.list();
+        if (list != null) {
+            for (String name : list) {
+                File newFile = new File(directory, name);
+                if (newFile.isDirectory()) {
+                    printPath(newFile);
+                    listDirectory(newFile);
+                } else {
+                    printPath(newFile);
+                }
+            }
+        }
+    }
+
+    private static void printPath(File newFile) {
+//        System.out.println(newFile.getPath());
+        try {
+            System.out.println(newFile.getCanonicalPath());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
