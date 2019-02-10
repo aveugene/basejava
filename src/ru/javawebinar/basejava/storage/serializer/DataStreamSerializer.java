e1b43684b908abe6ef55e3415c04e8c95e962b20
aveugene <eugeneavilov@gmail.com>
2019-02-10 07:46:55 +0700

    Lesson10. HW10. v2. writeList -> writeCollection. Write Map(contacts and sections) with method writeCollection. lambda arguments with no type and brackets.

--- src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
@@ -5,6 +5,7 @@ import ru.javawebinar.basejava.model.*;
 import java.io.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -16,15 +17,13 @@ public class DataStreamSerializer implements StreamSerializer {
             dataOutputStream.writeUTF(resume.getFullName());
 
             Map<ContactType, String> contacts = resume.getContacts();
-            dataOutputStream.writeInt(contacts.size());
-            for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
+            writeSection(dataOutputStream, contacts.entrySet(), entry -> {
                 dataOutputStream.writeUTF(entry.getKey().name());
                 dataOutputStream.writeUTF(entry.getValue());
-            }
+            });
 
             Map<SectionType, AbstractSection> sections = resume.getSections();
-            dataOutputStream.writeInt(sections.size());
-            for (Map.Entry<SectionType, AbstractSection> entry : sections.entrySet()) {
+            writeSection(dataOutputStream, sections.entrySet(), entry -> {
                 SectionType sectionType = entry.getKey();
                 AbstractSection section = entry.getValue();
                 dataOutputStream.writeUTF(sectionType.name());
@@ -35,15 +34,15 @@ public class DataStreamSerializer implements StreamSerializer {
                         break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
-                        writeSectionList(dataOutputStream, ((ListSection) section).getTexts(), dataOutputStream::writeUTF);
+                        writeSection(dataOutputStream, ((ListSection) section).getTexts(), dataOutputStream::writeUTF);
                         break;
                     case EXPERIENCE:
                     case EDUCATION:
-                        writeSectionList(dataOutputStream, ((CompaniesSection) section).getCompanies(), (Company company) -> {
+                        writeSection(dataOutputStream, ((CompaniesSection) section).getCompanies(), company -> {
                             Link link = company.getWebsiteLink();
                             dataOutputStream.writeUTF(link.getUrl());
                             dataOutputStream.writeUTF(link.getName());
-                            writeSectionList(dataOutputStream, company.getPeriods(), (Company.Period period) -> {
+                            writeSection(dataOutputStream, company.getPeriods(), period -> {
                                 writeDate(dataOutputStream, period.getStartDate());
                                 writeDate(dataOutputStream, period.getEndDate());
                                 dataOutputStream.writeUTF(period.getTitle());
@@ -52,7 +51,7 @@ public class DataStreamSerializer implements StreamSerializer {
                         });
                         break;
                 }
-            }
+            });
         }
     }
 
@@ -92,6 +91,11 @@ public class DataStreamSerializer implements StreamSerializer {
     }
 
     @FunctionalInterface
+    private interface WriteInterface<T> {
+        void write(T item) throws IOException;
+    }
+
+    @FunctionalInterface
     private interface ReadInterface {
         void read() throws IOException;
     }
@@ -101,24 +105,26 @@ public class DataStreamSerializer implements StreamSerializer {
         T read() throws IOException;
     }
 
-    @FunctionalInterface
-    private interface WriteListInterface<T> {
-        void write(T item) throws IOException;
-    }
-
     private void writeDate(DataOutputStream dataOutputStream, LocalDate date) throws IOException {
         dataOutputStream.writeInt(date.getYear());
         dataOutputStream.writeInt(date.getMonth().getValue());
     }
 
-    private LocalDate readDate (DataInputStream dataInputStream) throws IOException {
+    private LocalDate readDate(DataInputStream dataInputStream) throws IOException {
         return LocalDate.of(dataInputStream.readInt(), dataInputStream.readInt(), 1);
     }
 
-    private <T> void writeSectionList(DataOutputStream dataOutputStream, List<T> sectionList, WriteListInterface<T> writeListInterface) throws IOException {
-        dataOutputStream.writeInt(sectionList.size());
-        for (T item : sectionList) {
-            writeListInterface.write(item);
+    private <T> void writeSection(DataOutputStream dataOutputStream, Collection<T> collection, WriteInterface<T> writeInterface) throws IOException {
+        dataOutputStream.writeInt(collection.size());
+        for (T item : collection) {
+            writeInterface.write(item);
+        }
+    }
+
+    private void readSection(DataInputStream dataInputStream, ReadInterface readInterface) throws IOException {
+        int size = dataInputStream.readInt();
+        for (int i = 0; i < size; i++) {
+            readInterface.read();
         }
     }
 
@@ -131,10 +137,5 @@ public class DataStreamSerializer implements StreamSerializer {
         return list;
     }
 
-    private void readSection(DataInputStream dataInputStream, ReadInterface readInterface) throws IOException {
-        int size = dataInputStream.readInt();
-        for (int i = 0; i < size; i++) {
-            readInterface.read();
-        }
-    }
+
 }
