94afb201eea2610f77a66e49d16d688c87a89756
aveugene <eugeneavilov@gmail.com>
2019-03-20 05:01:46 +0700

    Lesson12. HW12. v2. Functions based on Streams. minValue - reduce instead of forEach and AtomicInteger. oddOrEven - extract sum to variable for single calculation.

--- src/ru/javawebinar/basejava/MainStreams.java
+++ src/ru/javawebinar/basejava/MainStreams.java
@@ -3,7 +3,6 @@ package ru.javawebinar.basejava;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -12,20 +11,19 @@ public class MainStreams {
         int[] testArr = {1, 2, 3, 2, 3, 3};
         System.out.println(minValue(testArr));
 
-        ArrayList<Integer> testList = new ArrayList<>(Arrays.asList(1, 2, 3, 1));
+        ArrayList<Integer> testList = new ArrayList<>(Arrays.asList(1, 6, 9));
         System.out.println(oddOrEven(testList));
     }
 
     private static int minValue(int[] values) {
-        AtomicInteger result = new AtomicInteger();
-        Arrays.stream(values).sorted().distinct().forEach(x -> result.set(result.get() * 10 + x));
-        return result.get();
+        return Arrays.stream(values).distinct().sorted().reduce((acc, x) -> acc * 10 + x).getAsInt();
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum = integers.stream().mapToInt(Integer::intValue).sum();
         return integers.stream()
                 .flatMap(x -> {
-                    switch (integers.stream().mapToInt(Integer::intValue).sum() % 2) {
+                    switch (sum % 2) {
                         case 0:
                             if (x % 2 == 1) {
                                 return Stream.of(x);
